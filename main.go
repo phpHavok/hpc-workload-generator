fd7f9ce50487cbbaedd9b01e4bdd261d1b13ceac
Jacob Chappell <chappellind@gmail.com>
2021-03-26 16:30:39 -0500

    Allow debug level for log to be specified as a CLI flag.

--- main.go
+++ main.go
@@ -23,9 +23,20 @@ func lockOSThread(cpuID int) error {
 }
 
 func main() {
-	scheduleFile := flag.String("schedule-file", "", "path to the schedule file to execute, or empty for stdin")
+	// Parse command-line flags
+	scheduleFile := flag.String("i", "", "path to the schedule file to execute, or empty for stdin")
+	debugLevel := flag.Uint("d", 4, "debug level (0-6) where higher numbers have higher verbosity")
 	cgroupsRootPath := flag.String("cgroups-root", "/sys/fs/cgroup", "path to the root of the cgroupsv1 hierarchy")
 	flag.Parse()
+	// Clamp debug level to valid range and set it
+	if log.Level(*debugLevel) < log.PanicLevel {
+		*debugLevel = uint(log.PanicLevel)
+	}
+	if log.Level(*debugLevel) > log.TraceLevel {
+		*debugLevel = uint(log.TraceLevel)
+	}
+	log.SetLevel(log.Level(*debugLevel))
+	// Load and execute the schedule
 	schedule, err := loadSchedule(*scheduleFile, *cgroupsRootPath)
 	if err != nil {
 		log.Fatalf("failed to load schedule: %v", err)
